68b3af70a7c099af413b9c616a1337b5bf6dc154
Cassie Jeng <59851290+cjeng8771@users.noreply.github.com>
2023-01-09 21:41:35 -0600

    finished qpsk tx and rx in python

--- qpsk_tx_rx.py
+++ qpsk_tx_rx.py
@@ -7,7 +7,9 @@
 # Libraries:
 import math
 import numpy as np
-import matplotlib as plt
+import matplotlib.pyplot as plt
+import matplotlib
+from scipy.signal import lfilter
 ###########################################
 
 ###########################################
@@ -45,15 +47,11 @@ def binary2mary(data, M):
         binvalues[i] = int(math.pow(2,start))
         start=start-1
         i = i + 1
-    print(binvalues)
     for each in data:
         newdata.append(int(each))
     newdata = np.array(newdata)
     temp = np.reshape(newdata, (int(length/log2M), log2M))
-    temp = np.transpose(temp)
     marydata = temp.dot(binvalues)
-    marydata = np.transpose(marydata)
-    print(marydata)
     return marydata
 
 # PURPOSE: convert input data stream to signal space values for
@@ -65,9 +63,7 @@ def lut(data, inputVec, outputVec):
     if len(inputVec) != len(outputVec):
         print('Input and Output vectors must have identical length')
     # Initialize output
-    data = np.array(data)
-    data_shape = data.shape
-    output = np.zeros(data_shape)
+    output = np.zeros(data.shape)
     # For each possible data value
     eps = np.finfo('float').eps
     for i in range(len(inputVec)):
@@ -80,16 +76,15 @@ def lut(data, inputVec, outputVec):
 
 def oversample(x, OS_Rate):
     # Initialize output
-    length = len(x)
+    length = len(x[0])
     x_s = np.zeros((1,length*OS_Rate))
-    x_s = np.transpose(x_s)
     # Fill in one out of every OS_Rate samples with the input values
     count = 0
     h = 0
-    for k in range(len(x_s)):
+    for k in range(len(x_s[0])):
         count = count + 1
-        if count == 8:
-            x_s[k] = x[h]
+        if count == OS_Rate:
+            x_s[0][k] = x[0][h]
             count = 0
             h = h + 1
     return x_s
@@ -99,7 +94,7 @@ def SRRC(alpha, N, Lp):
     ntemp = list(range(-N*Lp, N*Lp+1))
     n = []
     for each in ntemp:
-        n.append(each + math.pow(10,-9)) # UP TO HERE WORKS
+        n.append(each + math.pow(10,-9))
     # Plug into time domain formula for the SRRC pulse shape
     h = []
     coeff = 1/math.sqrt(N)
@@ -115,6 +110,27 @@ def SRRC(alpha, N, Lp):
         h.append(pulse)
     return h
 
+# PURPOSE: Plot an eye diagram of a signal
+# INPUT: y_s: vector of signal samples out of the matched filter
+#        N: the number of samples per symbol. Assumes that time 0 is at sample
+#        y_s[0]. If not, you must send in an offset integer.
+# OUTPUT: none
+matplotlib.rc('xtick', labelsize=16) 
+matplotlib.rc('ytick', labelsize=16)
+def plot_eye_diagram(y_s, N, offset=0):
+    start_indices = range(int(np.floor(N/2.0)) + offset - 1, len(y_s) - N, N)
+    time_vals     = np.arange(-0.5, 0.5+1.0/N, 1.0/N)
+
+    plt.figure()
+    for i, start_i in enumerate(start_indices):
+        plt.plot(time_vals, y_s[start_i:(start_i+N+1)], 'b-', linewidth=2)
+        
+    plt.xlabel(r'Time $t/T_s$', fontsize=16)
+    plt.xlim([-0.5, 0.5])
+    plt.ylabel('Matched Filter Output', fontsize=16)
+    plt.grid(True)
+    plt.show()
+
 # Purpose: Find the symbols which are closest in symbol space
 #          to the received signal space values.
 # INPUT: Received r hat values (output of matched filter),
@@ -124,19 +140,20 @@ def SRRC(alpha, N, Lp):
 # OUTPUT: m-ary symbol indices in 0...length(outputVec)-1
 def findClosestMary(r_hat, outputVec):
     symbols = len(outputVec[0])
-    data_out = np.ones((1, len(r_hat)))
+    data_out = np.ones((1,len(r_hat[0])))
     # For each r_hat value
-    for i in range(1, len(r_hat[0])):
+    for i in range(len(r_hat[0])):
         # Find the index of the symbol space value with the
         # lowest distance to this r_hat.
-        distances = r_hat[:,i]*np.ones((1,symbols))
+        r_hat_temp = r_hat[:,i].reshape((len(r_hat),1))
+        distances = r_hat_temp*np.ones((1,symbols))
         distances = np.subtract(distances, outputVec)
         distances = np.square(distances)
         distances = sum(distances) # sum values in each column
         min_val = min(distances)
         min_ind = np.where(distances == min_val)
         # Output values 0, 1, ..., length(outputVec)-1
-        data_out[i] = min_ind[0][0]
+        data_out[0][i] = min_ind[0][0]
     return data_out
 
 # Purpose: Convert M-ary data to binary data
@@ -152,7 +169,7 @@ def mary2binary(data, M):
     binarydata = np.zeros((1,length*log2M))
     count = 0
     for each in data:
-        binval = format(each, format_string)
+        binval = format(int(each), format_string)
         for i in range(log2M):
             binarydata[0][count+i] = int(binval[i])
         count = count + log2M
@@ -180,11 +197,11 @@ def binvector2str(binvector):
     bin_values = np.transpose(bin_values)
     str_out = '' # Initialize character vector
     for i in range(num_characters):
-        single_char = temp[i*7:i*7+7]
+        single_char = binvector[i*7:i*7+7]
         value = 0
         for counter in range(len(single_char)):
-            value = value + int(single_char[counter]) * int(bin_values[counter])
-        str_out[i] = chr(int(value))
+            value = value + (int(single_char[counter]) * int(bin_values[counter]))
+        str_out += chr(int(value))
     return str_out
 ###########################################
 
@@ -210,7 +227,6 @@ if 1:
     ### OUTPUT: 4-ary data (0..3) values
 
     data = binary2mary(data_bits, 4)
-    print(data)
 
     ###########################################
     ### Modulation
@@ -218,12 +234,13 @@ if 1:
     ### OUTPUT: modulated values, x
 
     A = math.sqrt(9/2)
-    A = int(A)
     inputVec   = [0, 1, 2, 3]
     outputVecI = [A, -A, A, -A]
     outputVecQ = [A, A, -A, -A]
     xI         = lut(data, inputVec, outputVecI)
     xQ         = lut(data, inputVec, outputVecQ)
+    xI = xI.reshape((1,len(data)))
+    xQ = xQ.reshape((1,len(data)))
 
     ###########################################
     ### Upsample
@@ -239,32 +256,30 @@ if 1:
     ### OUTPUT: baseband transmit signal s
 
     pulse = SRRC(0.5, 8, 6)
+    pulse = np.array(pulse)
+    pulse = np.reshape(pulse, pulse.size)
+    x_s_I = np.reshape(x_s_I, x_s_I.size)
+    x_s_Q = np.reshape(x_s_Q, x_s_Q.size)
     s_0_I = np.convolve(x_s_I, pulse, mode='full')
     s_0_Q = np.convolve(x_s_Q, pulse, mode='full')
-
+    
     ###########################################
     ### Up-convert
     ### INPUT: s, baseband signal
     ### OUTPUT: up_s, bandpass signal
 
     f_0 = 0.25
-    n = list(range(0, len(s_0_I)-1))
-    s_0_I = np.array(s_0_I)
-    s_0_Q = np.array(s_0_Q)
-    stemp = []
-    s = []
+    n = list(range(0, len(s_0_I)))
+    s = np.zeros((len(n),1))
     for each in n:
         val1 = math.sqrt(2) * s_0_I[each] * math.cos(2 * math.pi * f_0 * each)
         val2 = math.sqrt(2) * s_0_Q[each] * math.sin(2 * math.pi * f_0 * each)
-        stemp.append(val1 - val2)
-    randn = np.random.normal(loc=0.0, scale=1.0, size=stemp.shape)
-    randnn = []
-    for each in randn:
-        randnn.append(0.2 * each)
-    randnn = np.array(randnn)
-    stemp = np.array(stemp)
-    for i in range(len(stemp)-1):
-        s.append(stemp[i] + randnn[i])
+        s[each] = (val1 - val2)
+    # Adding random noise
+    randn = np.random.normal(loc=0.0, scale=1.0, size=s.shape)
+    for i in range(len(s)):
+        s[i] = s[i] + (0.2*randn[i])
+    s = np.reshape(s, (1,len(s)))
 
 ###########################################
 # Load from one of M. Rice's mat files. (in MATLAB version)
@@ -275,14 +290,18 @@ if 1:
 # Plot for project handout
 
 s = np.array(s)
+n = np.array(n)
+t = n.reshape((1,len(s[0])))
 plt.figure()
-plt.plot(s, marker = 'o', linestyle = 'dashed')
+plt.plot(t, s, 'ob-')
 plt.xlabel('Sample')
 plt.ylabel('Value')
+plt.title('Transmitted Sample with Noise')
 plt.grid()
 plt.show()
 
 ###########################################
+
 
 ###########################################
 
@@ -294,37 +313,41 @@ plt.show()
 # OUTPUT: s, baseband signal
 
 f_0 = 0.2499
-n = list(range(0, len(s)-1))
-s_rx_I = []
-s_rx_Q = []
+n = list(range(0, len(s[0])))
+s_rx_I = np.zeros((len(n), 1))
+s_rx_Q = np.zeros((len(n), 1))
 for each in n:
-    I_part = math.sqrt(2) * s[each] * math.cos(2 * math.pi * f_0 * each)
-    Q_part = -1 * math.sqrt(2) * s[each] * math.sin(2 * math.pi * f_0 * each)
-    s_rx_I.append(I_part)
-    s_rx_Q.append(Q_part)
+    I_part = math.sqrt(2) * s[0][each] * math.cos(2 * math.pi * f_0 * each)
+    Q_part = -1 * math.sqrt(2) * s[0][each] * math.sin(2 * math.pi * f_0 * each)
+    s_rx_I[each] = I_part
+    s_rx_Q[each] = Q_part
+s_rx_I = s_rx_I.reshape((1,len(s[0])))
+s_rx_Q = s_rx_Q.reshape((1,len(s[0])))
 
 ###########################################
 # Matched filter
 # INPUT: baseband transmitted signal s
 # OUTPUT: matched-filtered signal y
 pulse = SRRC(0.5, 8, 6)
-yI = filter(pulse, 1, s_rx_I)
-yQ = filter(pulse, 1, s_rx_Q)
+yI = lfilter(pulse, 1, s_rx_I)
+yQ = lfilter(pulse, 1, s_rx_Q)
 
 ###########################################
 # Plot for project handout
 
 plt.figure()
-plt.plot(yI, marker = 'o', linestyle = 'dashed')
+plt.plot(t, yI, 'ob-')
 plt.xlabel('Sample')
 plt.ylabel('Value')
+plt.title('yI Sample Plot')
 plt.grid()
 plt.show()
 
 plt.figure()
-plt.plot(yQ, marker = 'o', linestyle = 'dashed')
+plt.plot(t, yQ, 'ob-')
 plt.xlabel('Sample')
 plt.ylabel('Value')
+plt.title('yQ Sample Plot')
 plt.grid()
 plt.show()
 
@@ -333,11 +356,14 @@ plt.show()
 # INPUT: Matched Filter output
 # OUTPUT: Synched MF output with samples at US_Rate, 2*US_Rate, ...
 
-y_s_I = yI[97:]
-y_s_Q = yQ[97:]
+y_s_I = yI[0][96:]
+y_s_Q = yQ[0][96:]
 
 # Plot eye-diagram
-# Skipped for Python Version -- no found MATLAB code for function plot_eye_diagram in repository
+# plt.figure()
+hI = plot_eye_diagram(y_s_I, 8, 0)
+# plt.figure()
+hQ = plot_eye_diagram(y_s_Q, 8, 0)
 
 ###########################################
 # Downsample
@@ -346,21 +372,25 @@ y_s_Q = yQ[97:]
 
 US_Rate = 8
 length = len(y_s_I)
-r_hat_I = []
+r_hat_I = np.zeros((int(length/US_Rate),1))
+r_hat_Q = np.zeros((int(length/US_Rate),1))
 count = 0
+h = 0
 for k in range(length):
-	count = count + 1
-	if count == US_Rate:
-		r_hat_I.append(y_s_I[k])
-		count = 0
-r_hat_Q = []
+    count = count + 1
+    if count == US_Rate:
+        r_hat_I[h] = y_s_I[k]
+        count = 0
+        h = h + 1
 count = 0
+h = 0
 for k in range(length):
-	count = count + 1
-	if count == US_Rate:
-		r_hat_Q.append(y_s_Q[k])
-		count = 0
-expected_symbols = 84
+    count = count + 1
+    if count == US_Rate:
+        r_hat_Q[h] = y_s_Q[k]
+        count = 0
+        h = h + 1
+expected_symbols = len(data) #84
 if len(r_hat_I) > expected_symbols:
     r_hat_I = r_hat_I[:expected_symbols]
     r_hat_Q = r_hat_Q[:expected_symbols]
@@ -370,27 +400,28 @@ if len(r_hat_I) > expected_symbols:
 # INPUT: Symbol Samples
 # OUTPUT: Bits
 A = math.sqrt(9/2)
-outputVecI = [A, -A, A, -A]
-outputVecQ = [A, A, -A, -A]
-r_hat_I = np.array(r_hat_I)
-r_hat_Q = np.array(r_hat_Q)
+outputVecI = np.array([A, -A, A, -A])
+outputVecQ = np.array([A, A, -A, -A])
 r_hat = np.concatenate((r_hat_I, r_hat_Q), axis = 0)
-r_hat = np.reshape(r_hat, (2, len(r_hat_I)))
-outputVecI = np.array(outputVecI)
-outputVecQ = np.array(outputVecQ)
+
+r_hat_I = r_hat_I.reshape((1,expected_symbols))
+r_hat_Q = r_hat_Q.reshape((1,expected_symbols))
+r_hat = r_hat.reshape((2,expected_symbols))
+
 outputVec = np.concatenate((outputVecI, outputVecQ), axis = 0)
-outputVec = np.reshape(outputVec, (2, len(outputVecI)))
+outputVec = outputVec.reshape((2,4))
+
 symbols = len(r_hat[0])
-# symbols_out = np.zeros((1, symbols))
-# for i in range(symbols-1):
-#     symbols_out[i] = findClosest(r_hat[:,i], outputVec)
+
 symbols_out = findClosestMary(r_hat, outputVec)
+symbols_out = np.reshape(symbols_out, symbols_out.size)
 
 # Draw signal space constellation diagram
 plt.figure()
-plt.plot(r_hat[0,:], r_hat[1,:], linestyle = 'dotted')
+plt.plot(r_hat[0,:], r_hat[1,:], 'o')
 plt.xlabel('x_0')
 plt.ylabel('x_1')
+plt.title('Signal Space Constellation Diagram')
 plt.grid()
 plt.show()
 
