14b1cc0e46971281dd92801df5c4c70b29c83630
Jie Wang <jie.w@wustl.edu>
2023-01-17 14:54:35 -0600

    new measurements

---
+++ filetransfer.sh
@@ -0,0 +1,5 @@
+
+for HOST in wjcqmz@pc775.emulab.net wjcqmz@pc776.emulab.net wjcqmz@pc777.emulab.net wjcqmz@pc766.emulab.net wjcqmz@pc779.emulab.net wjcqmz@pc768.emulab.net wjcqmz@pc778.emulab.net
+do
+    scp QPSK_signal.iq $HOST:
+done
